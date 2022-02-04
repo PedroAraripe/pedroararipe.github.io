3a19071757ff7e80e0c7deefc819c9e891f22269
Lucas Araripe <pedro.lucx@gmail.com>
2022-02-03 21:39:11 -0300

    Starting project

--- src/pages/home/sections/HeroSection.js
+++ src/pages/home/sections/HeroSection.js
@@ -3,10 +3,11 @@ import React from 'react';
 import styled from 'styled-components'
 
 const TopMargin = styled.div`
-    margin-top:5%;
+    height: 100vh;
+    padding-top:5%;
     
     @media (min-width: 768px){
-        margin-top:15%;
+        padding-top:10%;
     }
 `
 
@@ -15,7 +16,7 @@ function HeroSection() {
         <TopMargin>
             <div class="h-100 d-flex flex-column align-items-between">
                 <section class="flex-grow-1">
-                    <div class="position-relative d-flex pb-5">
+                    <div className="pb-4 pb-md-5">
                         <div class="loader loader-cwise"></div>
                     </div>
                     <div>
@@ -24,7 +25,7 @@ function HeroSection() {
                     </div>
                     <div class="py-4">I'm a software engineer and artist based in Toronto. I have profound interest in full-stack development, machine learning, human-computer interactions, and everything in between.</div>
                 </section>
-                <div class="pb-5">
+                <div class="pb-3 pb-md-5">
                     <button class="border border-danger btn px-5 py-3 text-danger">Say hello</button>
                 </div>
             </div>
